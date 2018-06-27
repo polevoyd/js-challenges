a8acc5d47d829aa8d4d6e0ec22b00bed63ca903f
Dmitry <38082886+polevoyd@users.noreply.github.com>
2018-06-27 07:24:34 -0700

    Update js-challenges.js

--- js-challenges.js
+++ js-challenges.js
@@ -88,6 +88,33 @@ let reverse = function(x) {
     return ((r > 2147483647) || (r < -2147483648)) ? 0 : r;
 };
 /*-------------------------------------------------------------*/
+
+function differentSquares(matrix) {
+/*
+Given a rectangular matrix containing only digits, 
+calculate the number of different 2 × 2 squares in it.
+
+Example
+
+For
+
+matrix = [[1, 2, 1],
+          [2, 2, 2],
+          [2, 2, 2],
+          [1, 2, 3],
+          [2, 2, 1]]
+the output should be
+differentSquares(matrix) = 6.
+*/
+
+let squares = new Set();
+for (let i = 0; i < matrix.length - 1; i++)
+        for (let k = 0 ; k < matrix[i].length - 1 ; k++)
+                squares.add("s" + matrix[i][k] + matrix[i][k+1] + matrix[i+1][k] + matrix[i+1][k+1]);
+
+    return squares.size;
+}
+/*-------------------------------------------------------------*/
 /*-------------------------------------------------------------*/
 /*-------------------------------------------------------------*/
 /*-------------------------------------------------------------*/
