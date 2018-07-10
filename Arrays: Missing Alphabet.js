03595da42dbe732070146c0131697927fe77a94d
Dmitry <38082886+polevoyd@users.noreply.github.com>
2018-07-09 20:31:49 -0700

    Update Arrays: Missing Alphabet.js

--- Arrays: Missing Alphabet.js
+++ Arrays: Missing Alphabet.js
@@ -81,14 +81,4 @@ str = tmpArray.join('');
 }
 
 
-/*
-const insertMissingLetters = str => str
-  .split``
-  .map((e, i, a) => 
-    i === a.indexOf(e) ? e + 'abcdefghijklmnopqrstuvwxyz'
-      .split`` 
-      .filter(x => !str.includes(x) && x.charCodeAt() > e.charCodeAt())
-      .join``
-      .toUpperCase() : e)
-  .join``;  
-*/
+
