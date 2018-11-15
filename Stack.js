d6676b9c5929052b69ef0977bf8f47c624693b38
Dmitry <manxxeta@gmail.com>
2018-11-14 16:58:22 -0800

    algs

--- Stack.js
+++ Stack.js
@@ -46,15 +46,15 @@ class Stack {
 }
 
 
-// let stack = new Stack();
-// stack.push(1)
-// stack.push(2)
-// stack.push(3)
-// stack.push(4)
-// stack.push(5)
+let stack = new Stack();
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.push(4)
+stack.push(5)
 
-// stack.pop()
-// stack.pop()
-// stack.pop()
+stack.pop()
+stack.pop()
+stack.pop()
 
-// stack.top()
+stack.top()
