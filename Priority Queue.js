6f0efa082ffddfa12818a2f879be7fe8f24b112d
Dmitry <manxxeta@gmail.com>
2018-11-15 14:40:02 -0800

    trees

--- Priority Queue.js
+++ Priority Queue.js
@@ -1,5 +1,12 @@
-class PriorityQueue [
-    constructor(){
-        this.values = [];
+class Node {
+    constructor(value) {
+      this.value = value;
+      this.priority = this.priority;
     }
-]
+  }
+  
+  class PriorityQueue {
+      constructor(){
+          this.values = [];
+      }
+  }
