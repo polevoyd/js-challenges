36b1f0368a947ad7a8e675c4814099d697c71624
Dmitry <manxxeta@gmail.com>
2018-11-14 16:59:04 -0800

    algs

--- mergeSort.js
+++ mergeSort.js
@@ -1,28 +1,28 @@
 // helper function to merge back sorted arrays
 function mergeArr(first, second) {
-  let result = []
+  let result = [];
   let i = 0;
   let k = 0;
 
- while ((i < first.length) && (k < second.length)) {
-   if (first[i] > second[k]) {
-     result.push(second[k]);
-     k++;
-   } else {
-     result.push(first[i]);
-     i++;
-   }
- }
+  while ((i < first.length) && (k < second.length)) {
+    if (first[i] > second[k]) {
+      result.push(second[k]);
+      k++;
+    } else {
+      result.push(first[i]);
+      i++;
+    }
+  }
 
- while (i < first.length) {
-   result.push(first[i]);
-   i++;
- }
+  while (i < first.length) {
+    result.push(first[i]);
+    i++;
+  }
 
- while (k < second.length) {
-   result.push(second[k]);
-   k++;
- }
+  while (k < second.length) {
+    result.push(second[k]);
+    k++;
+  }
 
   return result;
 }
@@ -33,5 +33,5 @@ function mergeSort(arr) {
   let middle = Math.floor(arr.length/2);
   let left = mergeSort(arr.slice(0, middle));
   let right = mergeSort(arr.slice(middle));
-  return mergeArr(left, right)
+  return mergeArr(left, right);
 }
