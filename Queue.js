c08f47c9a01d73dd4f22c7b81e8006c336998c23
Dmitry <38082886+polevoyd@users.noreply.github.com>
2018-11-14 16:31:21 -0800

    Create Queue.js

---
+++ Queue.js
@@ -0,0 +1,59 @@
+class QueueNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  // add to tail
+  enqueue(value) {
+    let newNode = new QueueNode(value);
+    if (this.tail === null) {
+      this.tail = newNode;
+      this.head = newNode
+      this.length++;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+      this.length++;
+    }
+    return this;
+  }
+
+  // remove from head
+  dequeue() {
+    if (this.length === 0) { return null; }
+    let old = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+      this.length--;
+      return old.value;
+    } else {
+      this.head = old.next;
+      old.next = null
+      this.length--;
+      return old.value;
+
+    }
+  }
+}
+
+
+
+let queue = new Queue();
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+
+queue.dequeue();
+queue.dequeue();
+queue.dequeue();
+queue.dequeue();
