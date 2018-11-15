080cfcd395329729f13a457f9e1aa5cbb91f60db
Dmitry <manxxeta@gmail.com>
2018-11-15 13:34:28 -0800

    trees

--- Binary Heap.js
+++ Binary Heap.js
@@ -1,40 +1,39 @@
 
 class MaxBinaryHeap {
-    constructor(){
-      this.values = [];
-    }
-  
-    // insertion
-    insert(value) {
-      this.values.push(value);
-      this.bubbleUp();
-    }
-  
-    // helper bubble up function
-    bubbleUp() {
-      let index = this.values.length-1;
-      const value = this.values[index];
-      while (index > 0) {
-        let parentIndex = Math.floor((index-1)/2);
-        let parent = this.values[parentIndex];
-        if (value <= parent) break;
-        this.values[parentIndex] = value;
-        this.values[index] = parent;
-        index = parentIndex;
-      }
+  constructor(){
+    this.values = [];
+  }
+
+  // insertion
+  insert(value) {
+    this.values.push(value);
+    this.bubbleUp();
+  }
+
+  // helper bubble up function
+  bubbleUp() {
+    let index = this.values.length-1;
+    const value = this.values[index];
+    while (index > 0) {
+      let parentIndex = Math.floor((index-1)/2);
+      let parent = this.values[parentIndex];
+      if (value <= parent) break;
+      this.values[parentIndex] = value;
+      this.values[index] = parent;
+      index = parentIndex;
     }
   }
-  
-  let heap = new MaxBinaryHeap();
-  heap.insert(55);
-  heap.insert(12);
-  heap.insert(16);
-  heap.insert(13);
-  heap.insert(1);
-  heap.insert(5);
-  heap.insert(61);
-  heap.insert(27);
-  heap.insert(63);
-  
-  heap.values;
-  
+}
+
+let heap = new MaxBinaryHeap();
+heap.insert(55);
+heap.insert(12);
+heap.insert(16);
+heap.insert(13);
+heap.insert(1);
+heap.insert(5);
+heap.insert(61);
+heap.insert(27);
+heap.insert(63);
+
+heap.values;
