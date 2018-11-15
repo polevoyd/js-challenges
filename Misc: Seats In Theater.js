7344142a0f158ee55974cf1bd8d44ad0d9003442
Dmitry <manxxeta@gmail.com>
2018-11-14 17:01:29 -0800

    algs

--- Misc: Seats In Theater.js
+++ Misc: Seats In Theater.js
@@ -1,10 +1,10 @@
 function seatsInTheater(nCols, nRows, col, row) {
-    /*Given the total number of rows and columns in the theater (nRows and nCols, respectively), and the row and column you're sitting in, return the number of people who sit strictly behind you and in your column or to the left, assuming all seats are occupied.
+  /*Given the total number of rows and columns in the theater (nRows and nCols, respectively), and the row and column you're sitting in, return the number of people who sit strictly behind you and in your column or to the left, assuming all seats are occupied.
 
 Example
 
 For nCols = 16, nRows = 11, col = 5, and row = 3, the output should be
 seatsInTheater(nCols, nRows, col, row) = 96.*/
-    
-    return (nCols-col+1)*(nRows-row);
+
+  return (nCols-col+1)*(nRows-row);
 }
