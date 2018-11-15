e3c7009b33be1e4aecdce2ee855b475e383d8b26
Dmitry <manxxeta@gmail.com>
2018-11-15 08:45:38 -0800

    algs

--- Trees.js
+++ Trees.js
@@ -1,3 +1,12 @@
 // root - main node
 // non-linear data structure
 // only one root and only parent -> child relationship
+
+class bstNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
