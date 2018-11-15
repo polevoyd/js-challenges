0a525fca5d850c03a0200b7959b2e2a877c6fee1
Dmitry <manxxeta@gmail.com>
2018-11-15 09:07:02 -0800

    trees

--- Trees.js
+++ Trees.js
@@ -2,7 +2,7 @@
 // non-linear data structure
 // only one root and only parent -> child relationship
 
-class bstNode {
+class BSTNode {
   constructor(value) {
     this.value = value;
     this.left = null;
@@ -14,10 +14,50 @@ class BST {
   constructor() {
     this.root = null;
   }
+
+  // insert
+  insert(value) {
+    let newNode = new BSTNode(value);
+    let current = this.root;
+
+    if (!current) {
+      this.root = newNode;
+    } else {
+      while (current) {
+        if (value < current.value){
+          if (current.left){
+            current = current.left;
+          } else {
+            current.left = newNode;
+            return this; 
+          }
+          
+        } else {
+          if (current.right) {
+            current = current.right;
+          } else {
+            current.right = newNode;
+            return this;
+          }
+          
+        }
+      }
+    }
+
+  }
+
+
+
+
+
+
 }
 
 let tree = new BST();
-tree.root = new bstNode(10);
+tree.root = new BSTNode(10);
+tree.left = new BSTNode(5);
+tree.right = new BSTNode(14);
+tree.insert(12);
 
-
+console.log(tree);
 
