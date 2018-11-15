6586c2bbaf9283e716eb9e9ff769d535c6670ccf
Dmitry <manxxeta@gmail.com>
2018-11-14 16:59:41 -0800

    algs

--- Arrays: Find First Substring Occurrence.js
+++ Arrays: Find First Substring Occurrence.js
@@ -1,10 +1,10 @@
 function findFirstSubstringOccurrence(s, x) {
-    /*
-    Avoid using built-in functions to solve this challenge. Implement them yourself, 
+  /*
+    Avoid using built-in functions to solve this challenge. Implement them yourself,
     since this is what you would be asked to do during a real interview.
 
-Implement a function that takes two strings, s and x, as arguments and finds the first 
-occurrence of the string x in s. The function should return an integer indicating the 
+Implement a function that takes two strings, s and x, as arguments and finds the first
+occurrence of the string x in s. The function should return an integer indicating the
 index in s of the first occurrence of x. If there are no occurrences of x in s, return -1.
 
 Example
@@ -14,22 +14,22 @@ strstr(s, x) = -1;
 For s = "CodefightsIsAwesome" and x = "IsA", the output should be
 strstr(s, x) = 10.
     */
-    
+
 //////////////////////////////////////////////////////////////////////////
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
 //////////////////////////////////////////////////////////////////////////
-    // BRUTE FORCE
-    // TIME: N size * X size
-    // SPACE: 1 CONSTANT
-    /*
+  // BRUTE FORCE
+  // TIME: N size * X size
+  // SPACE: 1 CONSTANT
+  /*
     for (let i = 0; i < s.length; i++)
     {
         if (s[i] === x[0])
@@ -40,7 +40,7 @@ strstr(s, x) = 10.
                 if (x[k] !== s[i+k])
                     match = false;
             }
-            
+
             if (match === true)
                 return i;
         }
