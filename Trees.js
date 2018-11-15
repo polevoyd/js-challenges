d60913dfd06defb211c899462c930733e3efdb65
Dmitry <manxxeta@gmail.com>
2018-11-15 12:08:56 -0800

    trees

--- Trees.js
+++ Trees.js
@@ -104,6 +104,17 @@ class BST {
     traverse(this.root);
     return data;
   }
+
+  DFSInOrder(){
+    let data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      data.push(node.value);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  }
 }
 
 let tree = new BST();
