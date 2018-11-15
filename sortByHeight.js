2838cdd2a24bd43294b84a2132a09a912991ca6f
Dmitry <manxxeta@gmail.com>
2018-11-14 17:03:20 -0800

    algs

--- sortByHeight.js
+++ sortByHeight.js
@@ -9,23 +9,23 @@ sortByHeight(a) = [-1, 150, 160, 170, -1, -1, 180, 190].
 
 function sortByHeight(a) {
 
-    for (let i = 0 ; i < a.length ; i++)
-         for (let j = i ; j < a.length ; j++)
-              /* If its = -1 - we don't touch it */
-              if ( (a[j] < a[i]) && (a[i] !== -1) && (a[j] !== -1) ) 
-              {
-                let temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
-              }
+  for (let i = 0 ; i < a.length ; i++)
+    for (let j = i ; j < a.length ; j++)
+    /* If its = -1 - we don't touch it */
+      if ( (a[j] < a[i]) && (a[i] !== -1) && (a[j] !== -1) )
+      {
+        let temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+      }
 
-    return a;
-    
-    
-    /*
+  return a;
+
+
+  /*
      i = 0
 
      sortByHeight = a => a.map(v => v<0 ? v : b[i++], b = a.filter(v => v >= 0).sort((a,b) => a-b))
-     
+
      */
 }
