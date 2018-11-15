68c4fa6b69e81c68e83a6348f56ccd98db8f9885
Dmitry <manxxeta@gmail.com>
2018-11-15 09:50:16 -0800

    trees

--- Trees.js
+++ Trees.js
@@ -18,42 +18,48 @@ class BST {
   // insert
   insert(value) {
     let newNode = new BSTNode(value);
-    let current = this.root;
-
-    if (!current) {
+    if (this.root === null) {
       this.root = newNode;
+      return this;
     } else {
-      while (current) {
-        if (value < current.value){
-          if (current.left){
-            current = current.left;
-          } else {
+      let current = this.root;
+      while (true) {
+        if (value === current.value) {
+          return null;
+        }
+        if (value < current.value) {
+          if (current.left === null) {
             current.left = newNode;
-            return this; 
-          }
-          
-        } else {
-          if (current.right) {
-            current = current.right;
+            return this;
           } else {
+            current = current.left;
+          }
+        } else if (value > current.value) {
+          if (current.right === null) {
             current.right = newNode;
             return this;
+          } else {
+            current = current.right;
           }
-          
         }
       }
     }
-
   }
 
 
 }
 
 let tree = new BST();
-tree.root = new BSTNode(10);
-tree.left = new BSTNode(5);
-tree.right = new BSTNode(14);
+// tree.root = new BSTNode(10);
+// tree.left = new BSTNode(5);
+// tree.right = new BSTNode(14);
 tree.insert(12);
+tree.insert(4);
+tree.insert(8);
+tree.insert(61);
+tree.insert(7);
+tree.insert(23);
 
-console.log(tree);
+
+// console.log(tree);
 
