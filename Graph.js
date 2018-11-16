fb8d60ea92c67316f44ba9ee4968499c959936de
Dmitry <manxxeta@gmail.com>
2018-11-16 15:13:21 -0800

    graphs

--- Graph.js
+++ Graph.js
@@ -13,11 +13,18 @@ class Graph {
     this.adjacencyList[v1].push(v2);
     this.adjacencyList[v2].push(v1);
   }
+
+  removeEdge(v1, v2) {
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter(e => e !== v2);
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter(e => e !== v1);
+  }
 }
 
 //
 
 let graph = new Graph();
-graph.addVertex('first!');
+graph.addVertex('Krasnodar');
+graph.addVertex('Moscow');
+graph.addVertex('Rostov');
 
-graph.adjacencyList;
+
