0bd6aa1e6f1c2a40b84386cea50d6677bd1bf106
Dmitry <manxxeta@gmail.com>
2018-11-16 15:16:36 -0800

    graphs

--- Graph.js
+++ Graph.js
@@ -15,8 +15,8 @@ class Graph {
   }
 
   removeEdge(v1, v2) {
-    this.adjacencyList[v1] = this.adjacencyList[v1].filter(e => e !== v2);
-    this.adjacencyList[v2] = this.adjacencyList[v2].filter(e => e !== v1);
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter(v => v !== v2);
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter(v => v !== v1);
   }
 }
 
