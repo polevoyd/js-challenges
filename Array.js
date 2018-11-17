973cc12e58ec119519c3737c48534279f2e498ba
Dmitry <manxxeta@gmail.com>
2018-11-16 17:56:38 -0800

    graphs

--- Array.js
+++ Array.js
@@ -1,43 +1,16 @@
 /**
  *
- * • Constant-time access given the index – Index of each element
+ * + Constant-time access given the index – Index of each element
  * maps directly to a particular memory address.
- * • Space efficiency – Consist purely of data, so no space is wasted with
+ * + Space efficiency – Consist purely of data, so no space is wasted with
  * links or other formatting information.
- * • Memory locality – Excellent memory locality. Good for CPU caching.
+ * + Memory locality – Excellent memory locality. Good for CPU caching.
  * 
+ * - Slow insertion in head/index
  *          Head        Index       Tail
  * Insert   N           N           1
  * Remove   N           N           1
- * Find     N
- * 
- * 
- * 
- * 
+ * Search   N
  * 
  */
 
- /**
-  * Simple storage
-Adding but not deleting
-Serialization
-Quick lookups by index
-Easy conversion to C-style arrays
-Efficient traversal (contiguous CPU caching)
-Do not use for
-
-Insertion/deletion in the middle of the list
-Dynamically changing storage
-Non-integer indexing
-Time Complexity
-
-Operation	Time Complexity
-Insert Head	O(n)
-Insert Index	O(n)
-Insert Tail	O(1)
-Remove Head	O(n)
-Remove Index	O(n)
-Remove Tail	O(1)
-Find Index	O(1)
-Find Object	O(n)
-  */
