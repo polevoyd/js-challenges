0d6c01c6b22b56eddc45576286c2650eb85af8fd
Dmitry <manxxeta@gmail.com>
2018-11-17 13:05:38 -0800

    graphs

--- WeightedGraph.js
+++ WeightedGraph.js
@@ -1,5 +1,13 @@
 class WeightedGraph {
-    constructor() {
-        this.adl = {};
-    }
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(vertex1, vertex2, weight) {
+    this.adjacencyList[vertex1].push({node : vertex2, weight: weight})
+  }
 }
