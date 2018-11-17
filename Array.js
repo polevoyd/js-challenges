5152b416553d469c38af9739eade15f72253d600
Dmitry <manxxeta@gmail.com>
2018-11-16 20:26:04 -0800

    graphs

--- Array.js
+++ Array.js
@@ -56,4 +56,26 @@ function rotateMatrix(a) {
   return transposed;
 }
 
-// Sudoku Validator
+// Validate Anagram
+function validAnagram(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  }
+  const lookup = {};
+  for (let i = 0; i < first.length; i++) {
+    let letter = first[i];
+    // if letter exists, increment, otherwise set to 1
+    lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+  }
+  for (let i = 0; i < second.length; i++) {
+    let letter = second[i];
+    // can't find letter or letter is zero then it's not an anagram
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+  return true;
+}
+
