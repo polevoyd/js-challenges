e80fe39a46a021d81bc7e7136ecf764bc7262ae1
Dmitry <manxxeta@gmail.com>
2018-11-17 15:28:50 -0800

    graphs

--- Fibonacchi.js
+++ Fibonacchi.js
@@ -2,3 +2,17 @@ function fibonacci(num) {
   if (num <= 2) return 1;
   return fibonacci(num-1) + fibonacci(num-2);
 }
+
+function fibonacciMemo(num, memo = []) {
+  if (memo[num] !== undefined) return memo[num];
+  if (num <= 2) return 1;
+  const result = fibonacciMemo(num-1, memo) + fibonacciMemo(n-2, memo);
+  memo[num] = result;
+  return result;
+}
+
+
+
+console.log(fibonacci(20));
+
+
