5880f2aa9ae4c70a3a6368770d0b8492ec35b513
Dmitry <38082886+polevoyd@users.noreply.github.com>
2018-12-01 14:03:10 -0800

    Create 1.5 One away.js

---
+++ 1.5 One away.js
@@ -0,0 +1,6 @@
+/*
+One away : There are three types of edits that can be performed on strings: 
+insert a character, remove a character and replace a character. 
+Given two strings, write a function to check if they are one edit (or zero edits) away.
+*/
+
