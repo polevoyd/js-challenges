cc551c4bc9854d9786e63c9d7329ebd1470b6b9f
Dmitry <38082886+polevoyd@users.noreply.github.com>
2018-12-10 11:24:10 -0800

    Update Array: Maximum Subarray.js

--- Array: Maximum Subarray.js
+++ Array: Maximum Subarray.js
@@ -32,3 +32,24 @@ var maxSubArray = function(nums) {
     }
     return maxSum;
 };
+
+var maxSubArray = function(nums) {
+    
+    // Kadane’s Algorithm:
+    // Time: N
+    
+    let max_so_far = -Infinity;
+    let max_ending_here = 0;
+    
+    for (let i = 0; i < nums.length; i++) {
+        
+        max_ending_here = max_ending_here + nums[i];
+        
+        if (max_so_far < max_ending_here)
+            max_so_far = max_ending_here;
+        
+        if (max_ending_here < 0)
+            max_ending_here = 0;
+    }
+    return max_so_far;
+};
