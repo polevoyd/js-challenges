293e35344f0c40b8e96b2e1686430b8b0c37e594
Dmitry <38082886+polevoyd@users.noreply.github.com>
2018-12-09 16:48:18 -0800

    Create Array: Contains Duplicate.js

---
+++ Array: Contains Duplicate.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+/*
+Given an array of integers, find if the array contains any duplicates.
+
+Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+*/
+
+var containsDuplicate = function(nums) {
+    // просто создать сет с уникальными значениями
+    // и сравнить размер
+    return nums.length !== new Set(nums).size
+};
