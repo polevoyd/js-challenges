5601dbec33ffc7ccb51d65639c12142183886cf7
Dmitry <38082886+polevoyd@users.noreply.github.com>
2018-12-10 18:56:08 -0800

    Create Tree: Maximum Depth of Binary.js

---
+++ Tree: Maximum Depth of Binary.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+/*
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Note: A leaf is a node with no children.
+*/
+
+var maxDepth = (root) => {
+    
+    // if root not alive - return 0
+    if (!root) return 0;
+    a
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
+}
