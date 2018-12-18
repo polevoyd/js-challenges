31aa5a66d4943cb535f9b771e0ebc68f01ce8428
Dmitry <38082886+polevoyd@users.noreply.github.com>
2018-12-18 11:27:37 -0800

    Create Tree: Find all paths in a tree whose sum is some given num.js

---
+++ Tree: Find all paths in a tree whose sum is some given num.js
@@ -0,0 +1,9 @@
+
+/*
+
+Find all paths in a tree whose sum is some given num
+(paths may not start at root)
+
+If paths were to start at root only, then it was straightforward to check all paths from the root. But if paths can start anywhere, then its slightly more complex.
+
+*/
